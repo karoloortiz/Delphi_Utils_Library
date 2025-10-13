b077a2cdde7a04d50d5c81b9928dfb8faf3516a0
Karol Ortiz <zitrokarol@gmail.com>
2025-10-13 12:55:19 +0200

    feat: adds array helper filter

--- KLib.ArrayHelper.pas
+++ KLib.ArrayHelper.pas
@@ -1,3 +1,39 @@
+{
+  KLib Version = 3.0
+  The Clear BSD License
+
+  Copyright (c) 2020 by Karol De Nery Ortiz LLave. All rights reserved.
+  zitrokarol@gmail.com
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted (subject to the limitations in the disclaimer
+  below) provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+  * Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+  THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+}
+
 unit KLib.ArrayHelper;
 
 interface
@@ -8,44 +44,47 @@ uses
 type
   TArrayHelper<T> = record
   private
-    FItems: TArray<T>;
+    _items: TArray<T>;
   public
-    // conversioni implicite per usare direttamente un array
-    class operator Implicit(a: TArray<T>): TArrayHelper<T>;
-    class operator Implicit(a: TArrayHelper<T>): TArray<T>;
+    //set
+    class operator Implicit(value: TArrayHelper<T>): TArray<T>;
 
-    // metodo Filter stile LINQ
-    function Filter(const predicate: TFunc<T, Boolean>): TArray<T>;
+    //get
+    class operator Implicit(value: TArray<T>): TArrayHelper<T>;
+
+    function filter(const predicate: TFunc<T, Boolean>): TArray<T>;
   end;
 
 implementation
 
-{ TArrayHelper<T> }
-
-class operator TArrayHelper<T>.Implicit(a: TArray<T>): TArrayHelper<T>;
+class operator TArrayHelper<T>.Implicit(value: TArrayHelper<T>): TArray<T>;
 begin
-  Result.FItems := a;
+  Result := value._items;
 end;
 
-class operator TArrayHelper<T>.Implicit(a: TArrayHelper<T>): TArray<T>;
+class operator TArrayHelper<T>.Implicit(value: TArray<T>): TArrayHelper<T>;
 begin
-  Result := a.FItems;
+  Result._items := value;
 end;
 
 function TArrayHelper<T>.Filter(const predicate: TFunc<T, Boolean>): TArray<T>;
 var
-  item: T;
-  count: Integer;
+  newArray: TArray<T>;
+
+  _item: T;
+  _sizeNewArray: Integer;
 begin
-  SetLength(Result, Length(FItems));
-  count := 0;
-  for item in FItems do
-    if predicate(item) then
+  SetLength(newArray, Length(_items));
+  _sizeNewArray := 0;
+  for _item in _items do
+    if predicate(_item) then
     begin
-      Result[count] := item;
-      Inc(count);
+      Result[_sizeNewArray] := _item;
+      Inc(_sizeNewArray);
     end;
-  SetLength(Result, count);
+  SetLength(newArray, _sizeNewArray);
+
+  Result := newArray;
 end;
 
 end.
